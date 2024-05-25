53fd98b514d48a9a922816367854debbd90ae0ee
Gui_Mello <89108147+GuiMello83@users.noreply.github.com>
2024-05-25 09:32:37 -0300

    alt

--- Front/src/Cadastro.js
+++ Front/src/Cadastro.js
@@ -1,25 +1,23 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import './Cadastro.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./Cadastro.css";
 
-// Adicione os cabeçalhos de autenticação a todas as requisições
-axios.interceptors.request.use(config => {
-  // Adicione o cabeçalho de autorização com as credenciais
-  config.headers.Authorization = localStorage.getItem('token'); // Substitua 'token' pelo nome do item onde está armazenado o token de autenticação
-  
+axios.interceptors.request.use((config) => {
+  config.headers.Authorization = localStorage.getItem("token");
+
   return config;
 });
 
 function Cadastro() {
   const [fornecedor, setFornecedor] = useState({
-    nome: '',
-    endereco: '',
-    telefone: ''
+    nome: "",
+    endereco: "",
+    telefone: "",
   });
 
   const [produto, setProduto] = useState({
-    nome: '',
-    fornecedorId: ''
+    nome: "",
+    fornecedorId: "",
   });
 
   const handleFornecedorChange = (e) => {
@@ -35,17 +33,22 @@ function Cadastro() {
   const handleSubmitFornecedor = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:8080/fornecedores', fornecedor);
-      const fornecedorId = response.data.id; 
+      const response = await axios.post(
+        "http://localhost:8080/fornecedores",
+        fornecedor
+      );
+      const fornecedorId = response.data.id;
       alert(`Fornecedor cadastrado com sucesso! ID: ${fornecedorId}`);
       setFornecedor({
-        nome: '',
-        endereco: '',
-        telefone: ''
+        nome: "",
+        endereco: "",
+        telefone: "",
       });
     } catch (error) {
-      console.error('Erro ao cadastrar fornecedor:', error);
-      alert('Erro ao cadastrar fornecedor. Verifique o console para mais detalhes.');
+      console.error("Erro ao cadastrar fornecedor:", error);
+      alert(
+        "Erro ao cadastrar fornecedor. Verifique o console para mais detalhes."
+      );
     }
   };
 
@@ -53,29 +56,32 @@ function Cadastro() {
     e.preventDefault();
     try {
       if (isNaN(produto.fornecedorId)) {
-        alert('O ID do fornecedor deve ser um número.');
+        alert("O ID do fornecedor deve ser um número.");
         return;
       }
       const produtoPayload = {
         nome: produto.nome,
-        fornecedor: { id: produto.fornecedorId }
+        fornecedor: { id: produto.fornecedorId },
       };
-      const response = await axios.post('http://localhost:8080/produtos', produtoPayload);
-      alert('Produto cadastrado com sucesso!');
+      const response = await axios.post(
+        "http://localhost:8080/produtos",
+        produtoPayload
+      );
+      alert("Produto cadastrado com sucesso!");
       setProduto({
-        nome: '',
-        fornecedorId: ''
+        nome: "",
+        fornecedorId: "",
       });
     } catch (error) {
-      console.error('Erro ao cadastrar produto:', error);
+      console.error("Erro ao cadastrar produto:", error);
       if (error.response) {
-        console.error('Detalhes do erro:', error.response.data);
+        console.error("Detalhes do erro:", error.response.data);
       }
-      alert('Erro ao cadastrar produto. Verifique o console para mais detalhes.');
+      alert(
+        "Erro ao cadastrar produto. Verifique o console para mais detalhes."
+      );
     }
   };
-
-
 
   return (
     <div className="container">
@@ -84,15 +90,35 @@ function Cadastro() {
         <form onSubmit={handleSubmitFornecedor}>
           <div className="form-group">
             <label htmlFor="nomeFornecedor">Nome do Fornecedor:</label>
-            <input type="text" id="nomeFornecedor" name="nome" value={fornecedor.nome} onChange={handleFornecedorChange} required />
+            <input
+              type="text"
+              id="nomeFornecedor"
+              name="nome"
+              value={fornecedor.nome}
+              onChange={handleFornecedorChange}
+              required
+            />
           </div>
           <div className="form-group">
             <label htmlFor="enderecoFornecedor">Endereço:</label>
-            <input type="text" id="enderecoFornecedor" name="endereco" value={fornecedor.endereco} onChange={handleFornecedorChange} />
+            <input
+              type="text"
+              id="enderecoFornecedor"
+              name="endereco"
+              value={fornecedor.endereco}
+              onChange={handleFornecedorChange}
+            />
           </div>
           <div className="form-group">
             <label htmlFor="telefoneFornecedor">Telefone:</label>
-            <input type="text" id="telefoneFornecedor" name="telefone" value={fornecedor.telefone} onChange={handleFornecedorChange} required />
+            <input
+              type="text"
+              id="telefoneFornecedor"
+              name="telefone"
+              value={fornecedor.telefone}
+              onChange={handleFornecedorChange}
+              required
+            />
           </div>
           <button type="submit">Cadastrar Fornecedor</button>
         </form>
@@ -103,11 +129,25 @@ function Cadastro() {
         <form onSubmit={handleSubmitProduto}>
           <div className="form-group">
             <label htmlFor="nomeProduto">Nome do Produto:</label>
-            <input type="text" id="nomeProduto" name="nome" value={produto.nome} onChange={handleProdutoChange} required />
+            <input
+              type="text"
+              id="nomeProduto"
+              name="nome"
+              value={produto.nome}
+              onChange={handleProdutoChange}
+              required
+            />
           </div>
           <div className="form-group">
             <label htmlFor="fornecedorId">ID do Fornecedor:</label>
-            <input type="text" id="fornecedorId" name="fornecedorId" value={produto.fornecedorId} onChange={handleProdutoChange} required />
+            <input
+              type="text"
+              id="fornecedorId"
+              name="fornecedorId"
+              value={produto.fornecedorId}
+              onChange={handleProdutoChange}
+              required
+            />
           </div>
           <button type="submit">Cadastrar Produto</button>
         </form>
--- Front/src/TransacaoPage.js
+++ Front/src/TransacaoPage.js
@@ -1,48 +1,50 @@
-import React, { useState, useEffect } from 'react';
-import { Form, Button, Alert } from 'react-bootstrap';
-import axios from 'axios';
-import './Transacaopage.css';
-import gasBackground from './Imagens/gas.jpg';
+import React, { useState, useEffect } from "react";
+import { Form, Button, Alert } from "react-bootstrap";
+import axios from "axios";
+import "./Transacaopage.css";
+import gasBackground from "./Imagens/gas.jpg";
 
 function TransacaoPage() {
-  const [produtoId, setProdutoId] = useState('');
+  const [produtoId, setProdutoId] = useState("");
   const [quantidadeEntrada, setQuantidadeEntrada] = useState(0);
   const [quantidadeSaida, setQuantidadeSaida] = useState(0);
-  const [success, setSuccess] = useState('');
+  const [success, setSuccess] = useState("");
   const [produtos, setProdutos] = useState([]);
   const [fornecedores, setFornecedores] = useState([]);
-  const [selectedFornecedorId, setSelectedFornecedorId] = useState('');
+  const [selectedFornecedorId, setSelectedFornecedorId] = useState("");
   const [volumeEstoque, setVolumeEstoque] = useState(0);
-  const [estoqueAtual, setEstoqueAtual] = useState('');
+  const [estoqueAtual, setEstoqueAtual] = useState("");
 
   useEffect(() => {
     const fetchProdutos = async () => {
       try {
-        const response = await axios.get('http://localhost:8080/produtos');
-        console.log('Produtos recebidos:', response.data);  // Log de depuração
+        const response = await axios.get("http://localhost:8080/produtos");
+        console.log("Produtos recebidos:", response.data);
         setProdutos(response.data);
       } catch (error) {
-        console.error('Erro ao buscar produtos:', error);
+        console.error("Erro ao buscar produtos:", error);
       }
     };
 
     const fetchFornecedores = async () => {
       try {
-        const response = await axios.get('http://localhost:8080/fornecedores');
-        console.log('Fornecedores recebidos:', response.data);  // Log de depuração
+        const response = await axios.get("http://localhost:8080/fornecedores");
+        console.log("Fornecedores recebidos:", response.data);
         setFornecedores(response.data);
       } catch (error) {
-        console.error('Erro ao buscar fornecedores:', error);
+        console.error("Erro ao buscar fornecedores:", error);
       }
     };
 
     const fetchVolumeEstoque = async () => {
       try {
-        const response = await axios.get('http://localhost:8080/estoques/volume');
-        console.log('Volume de estoque recebido:', response.data);  // Log de depuração
+        const response = await axios.get(
+          "http://localhost:8080/estoques/volume"
+        );
+        console.log("Volume de estoque recebido:", response.data);
         setVolumeEstoque(response.data.volume);
       } catch (error) {
-        console.error('Erro ao buscar volume de estoque:', error);
+        console.error("Erro ao buscar volume de estoque:", error);
       }
     };
 
@@ -56,11 +58,11 @@ function TransacaoPage() {
   };
 
   const handleQuantidadeEntradaChange = (event) => {
-    setQuantidadeEntrada(parseInt(event.target.value));
+    setQuantidadeEntrada(parseInt(event.target.value, 10));
   };
 
   const handleQuantidadeSaidaChange = (event) => {
-    setQuantidadeSaida(parseInt(event.target.value));
+    setQuantidadeSaida(parseInt(event.target.value, 10));
   };
 
   const handleFornecedorChange = (event) => {
@@ -70,78 +72,120 @@ function TransacaoPage() {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      await axios.post('http://localhost:8080/estoques', {
+      const payload = {
         produto: { id: produtoId },
         entrada: quantidadeEntrada,
-        saida: quantidadeSaida
-      });
-      setSuccess('Transação cadastrada com sucesso!');
-      setProdutoId('');
+        saida: quantidadeSaida,
+      };
+      console.log("Enviando dados para o backend:", payload);
+      await axios.post("http://localhost:8080/estoques", payload);
+      setSuccess("Transação cadastrada com sucesso!");
+      setProdutoId("");
       setQuantidadeEntrada(0);
       setQuantidadeSaida(0);
-      setSelectedFornecedorId('');
+      setSelectedFornecedorId("");
     } catch (error) {
-      console.error('Erro ao cadastrar transação:', error);
+      console.error("Erro ao cadastrar transação:", error);
+      if (error.response) {
+        console.error("Detalhes do erro:", error.response.data);
+      }
     }
   };
 
   const handleBuscarEstoqueAtual = async () => {
     try {
-      const response = await axios.get(`http://localhost:8080/estoques/estoqueAtual?produtoId=${produtoId}`);
-      console.log('Estoque atual recebido:', response.data);  // Log de depuração
+      const response = await axios.get(
+        `http://localhost:8080/estoques/estoqueAtual?produtoId=${produtoId}`
+      );
+      console.log("Estoque atual recebido:", response.data);
       setEstoqueAtual(response.data.estoqueAtual);
     } catch (error) {
-      console.error('Erro ao buscar estoque atual:', error);
+      console.error("Erro ao buscar estoque atual:", error);
     }
   };
 
   return (
-    <div className="transacao-page" style={{ backgroundImage: `url(${gasBackground})` }}>
+    <div
+      className="transacao-page"
+      style={{ backgroundImage: `url(${gasBackground})` }}
+    >
       <div className="transacao-form">
         <h2>Cadastrar Nova Transação</h2>
         <p>Volume de Itens no Estoque: {volumeEstoque}</p>
         <Form onSubmit={handleSubmit}>
           <Form.Group controlId="fornecedorId">
             <Form.Label>Fornecedor</Form.Label>
-            <Form.Control as="select" value={selectedFornecedorId} onChange={handleFornecedorChange}>
+            <Form.Control
+              as="select"
+              value={selectedFornecedorId}
+              onChange={handleFornecedorChange}
+            >
               <option value="">Selecione um fornecedor</option>
-              {fornecedores.map(fornecedor => (
-                <option key={fornecedor.id} value={fornecedor.id}>{fornecedor.nome}</option>
+              {fornecedores.map((fornecedor) => (
+                <option key={fornecedor.id} value={fornecedor.id}>
+                  {fornecedor.nome}
+                </option>
               ))}
             </Form.Control>
           </Form.Group>
           <Form.Group controlId="produtoId">
             <Form.Label>Produto</Form.Label>
-            <Form.Control as="select" value={produtoId} onChange={handleProdutoIdChange}>
+            <Form.Control
+              as="select"
+              value={produtoId}
+              onChange={handleProdutoIdChange}
+            >
               <option value="">Selecione um produto</option>
-              {produtos.map(produto => (
-                <option key={produto.id} value={produto.id}>{produto.nome}</option>
+              {produtos.map((produto) => (
+                <option key={produto.id} value={produto.id}>
+                  {produto.nome}
+                </option>
               ))}
             </Form.Control>
           </Form.Group>
           <Form.Group controlId="quantidadeEntrada">
             <Form.Label>Quantidade de Entrada</Form.Label>
-            <Form.Control type="number" placeholder="Digite a quantidade de entrada" value={quantidadeEntrada} onChange={handleQuantidadeEntradaChange} />
+            <Form.Control
+              type="number"
+              placeholder="Digite a quantidade de entrada"
+              value={quantidadeEntrada}
+              onChange={handleQuantidadeEntradaChange}
+            />
           </Form.Group>
           <Form.Group controlId="quantidadeSaida">
             <Form.Label>Quantidade de Venda</Form.Label>
-            <Form.Control type="number" placeholder="Digite a quantidade de saída" value={quantidadeSaida} onChange={handleQuantidadeSaidaChange} />
+            <Form.Control
+              type="number"
+              placeholder="Digite a quantidade de saída"
+              value={quantidadeSaida}
+              onChange={handleQuantidadeSaidaChange}
+            />
           </Form.Group>
           <Button variant="primary" type="submit">
             Cadastrar Transação
           </Button>
         </Form>
-        {success && <Alert variant="success" className="mt-3">{success}</Alert>}
+        {success && (
+          <Alert variant="success" className="mt-3">
+            {success}
+          </Alert>
+        )}
       </div>
 
       <div className="estoque-form">
         <h2>Estoque Atual</h2>
         <Form.Group controlId="buscarEstoque">
           <Form.Label>Buscar Estoque por Produto</Form.Label>
-          <Form.Control as="select" value={produtoId} onChange={handleProdutoIdChange}>
+          <Form.Control
+            as="select"
+            value={produtoId}
+            onChange={handleProdutoIdChange}
+          >
             <option value="">Selecione um produto</option>
-            {produtos.map(produto => (
-              <option key={produto.id} value={produto.id}>{produto.nome}</option>
+            {produtos.map((produto) => (
+              <option key={produto.id} value={produto.id}>
+                {produto.nome}
+              </option>
             ))}
           </Form.Control>
           <Button variant="primary" onClick={handleBuscarEstoqueAtual}>
